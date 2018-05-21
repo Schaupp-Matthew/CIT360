75837c6bccc8b2108a251632d57827a157449d0c
Schaupp-Matthew <ccbsamatt@gmail.com>
2018-05-21 00:43:40 -0400

    Finished executors

--- src/Sandbox/Main.java
+++ src/Sandbox/Main.java
@@ -11,7 +11,6 @@ import cit360.sandbox.controller.ThreadController;
 import cit360.sandbox.model.CollectionsModel;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -88,20 +87,20 @@ public class Main {
         Runnable2 runnable4 = new Runnable2("Runnable4", model);
         Runnable2 runnable5 = new Runnable2("Runnable5", model);
         
-        //Start threadController thread
-        threadController.start();
-        //Start runnableController thread
-        (new Thread(runnableController)).start();
-        //Start threads manually
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-        //Start Runnables manually
-        (new Thread(runnable2)).start();
-        (new Thread(runnable3)).start();
-        (new Thread(runnable4)).start();
-        (new Thread(runnable5)).start();
+//        //Start threadController thread
+//        threadController.start();
+//        //Start runnableController thread
+//        (new Thread(runnableController)).start();
+//        //Start threads manually
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.start();
+//        //Start Runnables manually
+//        (new Thread(runnable2)).start();
+//        (new Thread(runnable3)).start();
+//        (new Thread(runnable4)).start();
+//        (new Thread(runnable5)).start();
         
         //Create Executors
         ExecutorService executorService = Executors.newFixedThreadPool(2);
@@ -110,35 +109,38 @@ public class Main {
         //Use ExecutorService to manage threads
         System.out.println("\n\n\nStart ExecutorService");
         executorService.execute(thread5);
+        executorService.execute(thread4);
+        executorService.execute(thread3);
+        executorService.execute(thread2);
+        executorService.execute(threadController);
+        
         
         //Shutdown executorService
-        System.out.println("Exit ExecutorService");
+        System.out.println("Exit ExecutorService\n\n\n");
         executorService.shutdown();
-//        
-//        for(int i = 0; i < 10; i++){
-//            if(i == 0) {
-//                //Use ScheduledExecutorService to manage threads
-//                System.out.println("\n\n\nStart ScheduledExecutorService loop");
-//                scheduledExecutorService.scheduleWithFixedDelay(thread5, 2000, 5000, TimeUnit.MILLISECONDS);
-//            }
-//            else if(i == 9){
-//                //Shutdown scheduledExecutorService
-//                System.out.println("Exit ScheduledExecutorService loop");
-//                scheduledExecutorService.shutdown();
-//            }
-//        }
         
-//        System.out.println("\n\n\nStart ExecutorService loop");
-//                executorService.execute(thread5);
-//                
-//        System.out.println("\n\n\nStart ScheduledExecutorService loop");
-//                scheduledExecutorService.scheduleWithFixedDelay(thread5, 2000, 5000, TimeUnit.MILLISECONDS);
-//                
-//                System.out.println("Exit ExecutorService loop");
-//                executorService.shutdown();
-//                
-//                System.out.println("Exit ScheduledExecutorService loop");
-//                scheduledExecutorService.shutdown();
+        //Use ScheduledExecutorService to manage threads
+        System.out.println("\n\n\nStart ScheduledExecutorService");
+        scheduledExecutorService.execute(runnable5);
+        scheduledExecutorService.execute(runnable4);
+        scheduledExecutorService.execute(runnable3);
+        scheduledExecutorService.execute(runnable2);
+        scheduledExecutorService.execute(runnableController);
+        
+        
+        //Shutdown ScheduledxecutorService
+        System.out.println("Exit ScheduledExecutorService\n\n\n");
+        scheduledExecutorService.shutdown();
+        
+        try {
+            while(!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+                System.out.println("\n\n******Executors not finished******\n\n");
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("Executor Service Await Termination Interrupted!");
+        }
+        
+        System.out.println("\n******Finished******");
         
     }
     
