2e910b5358202617359ef65d0deff38140747eca
Schaupp-Matthew <ccbsamatt@gmail.com>
2018-05-20 21:37:01 -0400

    Laptop

---
+++ src/cit360/sandbox/model/NewModel.java
@@ -0,0 +1,15 @@
+/*
+ * CIT 360 Sandbox Code
+ * Author:  Matthew Schaupp
+ */
+package cit360.sandbox.model;
+
+/**
+ *
+ * @author Matthew
+ */
+public class NewModel {
+    
+}
+
+//This is an attempt to configure laptop
