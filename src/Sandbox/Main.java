8f2c9de17f4ebc8d783176108e106621aa609bc3
Schaupp-Matthew <ccbsamatt@gmail.com>
2018-05-26 21:35:57 -0400

    Uncomment sections for submission.

--- src/Sandbox/Main.java
+++ src/Sandbox/Main.java
@@ -95,77 +95,77 @@ public class Main {
 //        collectionsController.collectionsTreeMap();
 
 
-//        //======================================================================
-//        //Uncomment to run Threads, Runnables, and Executors
-//        //======================================================================
-//        //Create instance of model
-//        CollectionsModel model = new CollectionsModel();
-//        //Create an instance variable of the Thread and Runnable classes
-//        ThreadController threadController = new ThreadController();
-//        RunnableController runnableController = new RunnableController();
-//        Thread2 thread2 = new Thread2("Thread2", model);
-//        Thread2 thread3 = new Thread2("Thread3", model);
-//        Thread2 thread4 = new Thread2("Thread4", model);
-//        Thread2 thread5 = new Thread2("Thread5", model);
-//        Runnable2 runnable2 = new Runnable2("Runnable2", model);
-//        Runnable2 runnable3 = new Runnable2("Runnable3", model);
-//        Runnable2 runnable4 = new Runnable2("Runnable4", model);
-//        Runnable2 runnable5 = new Runnable2("Runnable5", model);
-//        
-////        //Start threadController thread
-////        threadController.start();
-////        //Start runnableController thread
-////        (new Thread(runnableController)).start();
-////        //Start threads manually
-////        thread2.start();
-////        thread3.start();
-////        thread4.start();
-////        thread5.start();
-////        //Start Runnables manually
-////        (new Thread(runnable2)).start();
-////        (new Thread(runnable3)).start();
-////        (new Thread(runnable4)).start();
-////        (new Thread(runnable5)).start();
-//        
-//        //Create Executors
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
-//        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
-//        //Use executors to manage threads
-//        //Use ExecutorService to manage threads
-//        System.out.println("\n\n\nStart ExecutorService");
-//        executorService.execute(thread5);
-//        executorService.execute(thread4);
-//        executorService.execute(thread3);
-//        executorService.execute(thread2);
-//        executorService.execute(threadController);
-//        
-//        
-//        //Shutdown executorService
-//        System.out.println("Exit ExecutorService\n\n\n");
-//        executorService.shutdown();
-//        
-//        //Use ScheduledExecutorService to manage threads
-//        System.out.println("\n\n\nStart ScheduledExecutorService");
-//        scheduledExecutorService.execute(runnable5);
-//        scheduledExecutorService.execute(runnable4);
-//        scheduledExecutorService.execute(runnable3);
-//        scheduledExecutorService.execute(runnable2);
-//        scheduledExecutorService.execute(runnableController);
-//        
-//        
-//        //Shutdown ScheduledxecutorService
-//        System.out.println("Exit ScheduledExecutorService\n\n\n");
-//        scheduledExecutorService.shutdown();
-//        
-//        try {
-//            while(!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
-//                System.out.println("\n\n******Executors not finished******\n\n");
-//            }
-//        } catch (InterruptedException ex) {
-//            System.out.println("\nExecutor Service Await Termination Interrupted!\n" + ex);
-//        }
-//        
-//        System.out.println("\n******Finished******");
+        //======================================================================
+        //Uncomment to run Threads, Runnables, and Executors
+        //======================================================================
+        //Create instance of model
+        CollectionsModel model = new CollectionsModel();
+        //Create an instance variable of the Thread and Runnable classes
+        ThreadController threadController = new ThreadController();
+        RunnableController runnableController = new RunnableController();
+        Thread2 thread2 = new Thread2("Thread2", model);
+        Thread2 thread3 = new Thread2("Thread3", model);
+        Thread2 thread4 = new Thread2("Thread4", model);
+        Thread2 thread5 = new Thread2("Thread5", model);
+        Runnable2 runnable2 = new Runnable2("Runnable2", model);
+        Runnable2 runnable3 = new Runnable2("Runnable3", model);
+        Runnable2 runnable4 = new Runnable2("Runnable4", model);
+        Runnable2 runnable5 = new Runnable2("Runnable5", model);
+        
+//        //Start threadController thread
+//        threadController.start();
+//        //Start runnableController thread
+//        (new Thread(runnableController)).start();
+//        //Start threads manually
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.start();
+//        //Start Runnables manually
+//        (new Thread(runnable2)).start();
+//        (new Thread(runnable3)).start();
+//        (new Thread(runnable4)).start();
+//        (new Thread(runnable5)).start();
+        
+        //Create Executors
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
+        //Use executors to manage threads
+        //Use ExecutorService to manage threads
+        System.out.println("\n\n\nStart ExecutorService");
+        executorService.execute(thread5);
+        executorService.execute(thread4);
+        executorService.execute(thread3);
+        executorService.execute(thread2);
+        executorService.execute(threadController);
+        
+        
+        //Shutdown executorService
+        System.out.println("Exit ExecutorService\n\n\n");
+        executorService.shutdown();
+        
+        //Use ScheduledExecutorService to manage threads
+        System.out.println("\n\n\nStart ScheduledExecutorService");
+        scheduledExecutorService.execute(runnable5);
+        scheduledExecutorService.execute(runnable4);
+        scheduledExecutorService.execute(runnable3);
+        scheduledExecutorService.execute(runnable2);
+        scheduledExecutorService.execute(runnableController);
+        
+        
+        //Shutdown ScheduledxecutorService
+        System.out.println("Exit ScheduledExecutorService\n\n\n");
+        scheduledExecutorService.shutdown();
+        
+        try {
+            while(!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+                System.out.println("\n\n******Executors not finished******\n\n");
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("\nExecutor Service Await Termination Interrupted!\n" + ex);
+        }
+        
+        System.out.println("\n******Finished******");
         
         
         //======================================================================
